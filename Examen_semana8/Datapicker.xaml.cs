8cb12b44930f1ef209deebd8aebd914477193a47
Rassmir <rassmir.flores@tecsup.edu.pe>
2019-09-30 16:43:45 -0500

    Examen Xamarin 2

---
+++ Examen_semana8/Datapicker.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Examen_semana8
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Datapicker : ContentPage
+    {
+        public Datapicker()
+        {
+            InitializeComponent();
+            dtp.Date = DateTime.Now;
+            dtp.MinimumDate = new DateTime(1999, 01, 01);
+            dtp.MaximumDate = new DateTime(2019, 12, 01);
+            dtp.DateSelected += DtpOnDateSelected;
+        }
+        private void DtpOnDateSelected(object sender, DateChangedEventArgs e)
+        {
+            DisplayAlert("Fecha", e.NewDate.ToString(("dd/MM/yyyy")), "OK");
+        }
+    }
+}
---
+++ Examen_semana8/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Examen_semana8
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Stacklayout());
+
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Grid());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new HierchicalNavigation());
+
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Tabbed());
+
+            };
+            Item5.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Formulario());
+
+            };
+            Item6.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Datapicker());
+
+            };
+
+        }
+    }
+}
